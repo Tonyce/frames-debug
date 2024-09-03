611ffb73b7898b224c2eb8ae206802befa6c0577
ttang <ttang@ttang-MacBook-Pro-2021.local>
2024-09-03 09:26:19 +0800

    init

---
+++ app/curationnft/frames/route.tsx
@@ -0,0 +1,59 @@
+/* eslint-disable react/jsx-key */
+import React from "react";
+import { Button } from "frames.js/next";
+import { frames, imageOptions } from "./frames";
+import { DEGENCAST_API, DEGENCAST_WEB_URL, FRAMES_BASE_URL } from "@/lib/env";
+import DegencastTag from "@/app/components/DegencastTag";
+import { error } from "frames.js/core";
+
+const handleRequest = frames(async (ctx) => {
+  const inviteFid = ctx.searchParams?.inviteFid || "";
+  const castHash = ctx.searchParams?.castHash || "";
+
+  let tokenId;
+  let communityCuration;
+  let castInfo;
+  let nftTokenUnit;
+  let launchProgress;
+
+  try {
+    const castInfoResp = await fetch(
+      `${DEGENCAST_API}/topics/casts/${castHash}/mint`
+    );
+    castInfo = await castInfoResp.json();
+  } catch (err) {
+    throw error("Error fetching castInfo");
+  }
+  launchProgress = castInfo?.data?.launchProgress || "0%";
+  nftTokenUnit = castInfo?.data?.nftTokenUnit;
+
+  console.log("castInfo", castInfo);
+  console.log(
+    "img",
+    `${DEGENCAST_API}/3r-farcaster/cast-image?castHash=${castHash}`,
+    `${FRAMES_BASE_URL}/images/degecasthat.png`
+  );
+
+  return {
+    // image: `${DEGENCAST_API}/3r-farcaster/cast-image?castHash=${castHash}`,
+    image: (
+      <div tw="flex relative">
+        <img
+          tw=""
+          src={`${DEGENCAST_API}/3r-farcaster/cast-image?castHash=${castHash}`}
+          alt=""
+        />
+        <DegencastTag
+          tokenUint={nftTokenUnit || "100000"}
+          progress={launchProgress}
+        />
+        <div tw="flex absolute">{Date.now()}</div>
+      </div>
+    ),
+    imageOptions: imageOptions,
+    buttons: [],
+  };
+});
+
+export const GET = handleRequest;
+export const POST = handleRequest;
---
+++ lib/env.ts
@@ -0,0 +1,34 @@
+export const FRAMES_BASE_URL = process.env.FRAMES_BASE_URL;
+export const DEGENCAST_API = process.env.DEGENCAST_API;
+
+export const FEE_RECIPIENT_WALLET_ADDRESS =
+  process.env.FEE_RECIPIENT_WALLET_ADDRESS;
+export const BUY_TOKEN_PERCENTAGE_FEE = process.env.BUY_TOKEN_PERCENTAGE_FEE;
+export const API_KEY_0X_API_KEY = process.env.API_KEY_0X_API_KEY;
+
+export const SWAPTOKEN_LEADERBOARD =
+  process.env.SWAPTOKEN_LEADERBOARD || "https://dev.degencast.xyz";
+
+export const DEGENCAST_WEB_URL =
+  process.env.DEGENCAST_WEB_URL || "https://dev.degencast.wtf";
+
+export const FARCASTER_HUB_URL = process.env.FARCASTER_HUB_URL;
+export const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY || "";
+
+export const TRADE_TOKEN_VIEWMORE =
+  process.env.TRADE_TOKEN_VIEWMORE ||
+  "https://dev.degencast.xyz/channels/tokens";
+export const TRADE_TOKEN_LEADERBOARD =
+  process.env.TRADE_TOKEN_LEADERBOARD ||
+  "https://dev.degencast.xyz/channels/tokens";
+
+export const BASE_NETWORK = process.env.BASE_NETWORK || "sepolia";
+export const DEGEN_ADDRESS: `0x${string}` = process.env
+  .DEGEN_ADDRESS as `0x${string}`;
+export const CURATION_FACTORY_ADDRESS: `0x${string}` = process.env
+  .CURATION_FACTORY_ADDRESS as `0x${string}`;
+export const UNISWAP_QUOTEV2_ADDRESS: `0x${string}` = process.env
+  .UNISWAP_QUOTEV2_ADDRESS as `0x${string}`;
+
+export const OPENSEA_BASE_URL = process.env.OPENSEA_BASE_URL;
+export const CURATION_ACTION_LINK = process.env.CURATION_ACTION_LINK;
